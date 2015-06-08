9055bd48eaae26cae2c1932feb28300b72704009
vlad pircioiu <parcioiuvlad@yahoo.com>
2015-06-08 18:10:00 +0300

    mainController small rest fix

--- src/main/webapp/resources/controllers/mainController.js
+++ src/main/webapp/resources/controllers/mainController.js
@@ -1,6 +1,6 @@
 app.controller('mainController', ['$scope', '$http', 'testRest', function($scope, $http, testRest) {
     $scope.click = function() {
-         $http.get('http://localhost:8080/web-app/testRest').success(function(data) {
+         $http.get('/web-app/testRest').success(function(data) {
               console.log(data);
               $scope.person = data;
          });
